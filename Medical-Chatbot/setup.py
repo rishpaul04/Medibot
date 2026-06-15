1bad7070ed7246a9f438f47b3d45406a5f308deb
Rishita Paul <154745264+rishpaul04@users.noreply.github.com>
2026-06-16 00:24:46 +0530

    Delete Medical-Chatbot directory

--- Medical-Chatbot/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name="medical-chatbot",
-    version="0.1.0",
-    author="Rishita Paul",
-    author_email="rishitapaul2004@gmail.com",
-    packages=find_packages(),
-    install_requires=[]
-)
